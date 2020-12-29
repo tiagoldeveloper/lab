433742736daacefefbe60135705748204aa8b336
Your Name <ldeveloper>
2020-12-29 17:08:09 -0300

    #refatorando.

--- src/main/java/br/com/lab/security/SecurityConfig.java
+++ src/main/java/br/com/lab/security/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**","/configuration/security", "/swagger-ui.html","/webjars/**").permitAll();
 
         var grupos = permissaoRepository.recupera(Grupo.class);
 
